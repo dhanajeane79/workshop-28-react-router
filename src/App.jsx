7c17fc709ab8245203d6511f0a2b960f3e4f4473
dhanajeane79 <dhanajeane79@gmail.com>
2023-07-30 15:49:29 -0400

    Added Blue and Red route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-  <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
-    </div>
+      <div id="container">
+        <h1></h1>
+        <div id="navbar"></div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<h1>Blue</h1>} />
+            <Route path="/red" element={<h1>Red</h1>} />
+          </Routes>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
