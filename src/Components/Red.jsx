8aea7fb035a73502b09a66bb741f97f6d0febc2f
dhanajeane79 <dhanajeane79@gmail.com>
2023-07-31 09:55:29 -0400

    Fixed Syntax error

--- src/Components/Red.jsx
+++ src/Components/Red.jsx
@@ -1,7 +1,12 @@
-export default function Red() {
-    return (
-      <div className="red">
-        <h1>RED</h1>
-      </div>
-    );
-  }
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Red() {
+  return (
+    <div className="red">
+      <h1>RED</h1>
+    </div>
+  );
+}
+
+export default Red;
