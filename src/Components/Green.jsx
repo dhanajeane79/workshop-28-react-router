b4005c56612ffcd946edf2d2b461f6030e45c91d
dhanajeane79 <dhanajeane79@gmail.com>
2023-07-31 10:48:25 -0400

    Created Green component

---
+++ src/Components/Green.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Green() {
+  return (
+    <div className="green">
+      <h1>GREEN</h1>
+    </div>
+  );
+}
+
+export default Green;
