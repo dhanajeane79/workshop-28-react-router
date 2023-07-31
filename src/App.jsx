7fd4edc7d7a2773a9b18829e2659c460e6b050fd
dhanajeane79 <dhanajeane79@gmail.com>
2023-07-31 09:53:24 -0400

    Replaced navbar and main-section divs with separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Blue from "./Components/Blue";
-import Red from "./Components/Red";
-import Home from "./Components/Home";
+import React from "react";
+
+
+import NaviBar from "./Components/NaviBar";
+import MainContainer from "./Components/MainContainer";
 import "./App.css";
 import "./index.css";
 
@@ -13,18 +13,8 @@ function App() {
     <>
       <div id="container">
         <h1></h1>
-        <div id="navbar">
-          <Link to="/blue">Blue</Link>
-          <Link to="/red">Red</Link>
-          <Link to="/home">Home</Link>
-        </div>
-        <div id="main-section">
-          <Routes>
-            <Route path="/blue" element={<Blue />} />
-            <Route path="/red" element={<Red />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
+        <NaviBar /> 
+        <MainContainer /> 
       </div>
     </>
   );
