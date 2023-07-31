abb33641bfe0542cb513409c656dd382521e0b05
dhanajeane79 <dhanajeane79@gmail.com>
2023-07-31 10:51:53 -0400

    Added routes/links/css for new color components

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Blue from "./Blue";
 import Red from "./Red";
+import Green from "./Green";
+import Yellow from "./Yellow";
+import Orange from "./Orange"; 
 import Home from "./Home";
 function MainContainer() {
   return (
@@ -10,7 +13,10 @@ function MainContainer() {
       <Routes>
         <Route path="/blue" element={<Blue />} />
         <Route path="/red" element={<Red />} />
+        <Route path="/green" element={<Green />} /> 
+        <Route path="/yellow" element={<Yellow />} /> 
         <Route path="/home" element={<Home />} />
+        <Route path="/orange" element={<Orange />} />
       </Routes>
     </div>
   );
--- src/Components/NaviBar.jsx
+++ src/Components/NaviBar.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 function NaviBar() {
   return (
     <div id="navbar">
-      <Link to="/blue">Blue</Link>
       <Link to="/red">Red</Link>
+      <Link to="/orange">Orange</Link>
+      <Link to="/yellow">Yellow</Link>
+      <Link to="/green">Green</Link>
+      <Link to="/blue">Blue</Link>
       <Link to="/home">Home</Link>
     </div>
   );
