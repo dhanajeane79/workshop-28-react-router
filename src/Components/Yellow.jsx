0b47039a77f8a452277cd6433c41d5ad1de660bb
dhanajeane79 <dhanajeane79@gmail.com>
2023-07-31 10:49:54 -0400

    Created Yellow component

---
+++ src/Components/Yellow.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Yellow() {
+  return (
+    <div className="yellow">
+      <h1>YELLOW</h1>
+    </div>
+  );
+}
+
+export default Yellow;
