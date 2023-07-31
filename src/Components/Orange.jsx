07efff989e5e2034388aa6e7c26e0ce696eec9ee
dhanajeane79 <dhanajeane79@gmail.com>
2023-07-31 10:49:19 -0400

    Created Orange component

---
+++ src/Components/Orange.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Orange() {
+  return (
+    <div className="orange">
+      <h1>ORANGE</h1>
+    </div>
+  );
+}
+
+export default Orange;
